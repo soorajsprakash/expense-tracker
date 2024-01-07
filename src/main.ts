cfa03eb655d589be850e34296d0656e1f26acd93
Sooraj S Prakash <soorajsprakash007@gmail.com>
2024-01-07 20:41:00 +0530

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  }
+})
+
+const app = createApp(App)
+
+app.use(vuetify)
+app.mount('#app')
