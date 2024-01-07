f1e980c86832c29e55d6fcad062ecfe6bd6fdbae
Sooraj S Prakash <soorajsprakash007@gmail.com>
2024-01-07 23:34:39 +0530

    add balance and incomeExpenses

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 	<v-layout class="rounded rounded-md">
 		<v-app-bar color="surface-variant" title="Expense Tracker"></v-app-bar>
 
-		<v-navigation-drawer location="right">
-			<h3>History</h3>
-		</v-navigation-drawer>
-
 		<v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-			<Balance />
+			<div class="container">
+				<Balance/>
+				<br>
+				<IncomeExpense/>
+			</div>
 		</v-main>
 	</v-layout>
 </template>
@@ -15,6 +15,5 @@
 
 <script setup lang="ts">
 import Balance from './components/Balance.vue';
-
-
+import IncomeExpense from './components/IncomeExpense.vue';
 </script>
---
+++ src/components/IncomeExpense.vue
@@ -0,0 +1,14 @@
+<template>
+	<v-card width="400px" class="inc-expense-container">
+		<v-row no-gutters class="justify-center align-center mx-auto">
+            <v-card-item>
+                <v-card-subtitle>Income</v-card-subtitle>
+                <v-card-title id="money-plus" class="money plus">+ 0.00₹</v-card-title>
+            </v-card-item>
+            <v-card-item>
+                <v-card-subtitle>Expense</v-card-subtitle>
+                <v-card-title id="money-minus" class="money minus">- 0.00₹</v-card-title>
+            </v-card-item>
+        </v-row>
+	</v-card>
+</template>
